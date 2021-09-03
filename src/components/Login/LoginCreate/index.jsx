bbb21de487ef17a064be0b905d73d32eb69fc574
Douglas Oliveira <douglaspo_97@outlook.com>
2021-09-03 19:40:59 -0300

    Estilizando componente Login Create e criando função de criar novos usuários

--- src/components/Login/LoginCreate/index.jsx
+++ src/components/Login/LoginCreate/index.jsx
@@ -1,7 +1,39 @@
+import { useContext } from 'react'
+
+import { Input } from '../../Forms/Input'
+import { Button } from '../../Forms/Button'
+
+import { useForm } from '../../../hooks/useForm'
+import { USER_POST } from '../../../api/api'
+import { UserContext } from '../../../context/UserContext'
+
 export function LoginCreate() {
+  const username = useForm()
+  const email = useForm('email')
+  const password = useForm()
+
+  const { userLogin } = useContext(UserContext)
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value
+    })
+    const response = await fetch(url, options)
+    if (response.ok) userLogin(username.value, password.value)
+  }
+
   return (
-    <div>
-      <h1>Login Create</h1>
-    </div>
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Password" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
   )
 }
