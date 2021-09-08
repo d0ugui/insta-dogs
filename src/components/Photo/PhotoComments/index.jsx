a03d3a819136ca02dfe24dd663fd8964aa07a9d2
Douglas Oliveira <douglaspo_97@outlook.com>
2021-09-08 17:25:21 -0300

    Adicionando funcionalidade de comentário no post

--- src/components/Photo/PhotoComments/index.jsx
+++ src/components/Photo/PhotoComments/index.jsx
@@ -1,7 +1,25 @@
-export function PhotoComments() {
+import { useContext, useState } from 'react'
+
+import { UserContext } from '../../../context/UserContext'
+import { PhotoCommentsForm } from '../PhotoCommentsForm'
+
+import { Items } from './styles'
+
+export function PhotoComments(props) {
+  const [comments, setComments] = useState(() => props.comments)
+  const { login } = useContext(UserContext)
+
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <>
+      <Items>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </Items>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
   )
 }
---
+++ src/components/Photo/PhotoCommentsForm/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+import { useFetch } from '../../../hooks/useFetch'
+import { COMMENT_POST } from '../../../api/api'
+import { Error } from '../../Helper'
+
+import { ReactComponent as Image } from '../../../assets/enviar.svg'
+
+export function PhotoCommentsForm({ id, setComments }) {
+  const [comment, setComment] = useState('')
+  const { request, error } = useFetch()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const { url, options } = COMMENT_POST(id, { comment })
+    const { response, json } = await request(url, options)
+    if (response.ok) {
+      setComment('')
+      setComments((comments) => [...comments, json])
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente"
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      ></textarea>
+      <button>
+        <Image />
+      </button>
+      <Error error={error} />
+    </form>
+  )
+}
