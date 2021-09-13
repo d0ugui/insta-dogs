2d93eb0d19e9b31bc8c3c304f01e1f92e565da44
Douglas Oliveira <douglaspo_97@outlook.com>
2021-09-13 15:13:15 -0300

    Adicionando funcionalidade de perdeu a senha

--- src/components/Login/LoginPasswordLost/index.jsx
+++ src/components/Login/LoginPasswordLost/index.jsx
@@ -1,7 +1,43 @@
+import { Input } from '../../Forms/Input'
+import { Button } from '../../Forms/Button'
+import { useForm } from '../../../hooks/useForm'
+import { useFetch } from '../../../hooks/useFetch'
+import { PASSWORD_LOST } from '../../../api/api'
+import { Error } from '../../Helper'
+
 export function LoginPasswordLost() {
+  const login = useForm()
+  const { data, loading, error, request } = useFetch()
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace('perdeu', 'resetar')
+      })
+      const { json } = await request(url, options)
+      console.log(json)
+    }
+  }
+
   return (
-    <div>
-      <h1>Login Password Lost</h1>
-    </div>
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar email</Button>
+          )}
+        </form>
+      )}
+
+      <Error error={error} />
+    </section>
   )
 }
